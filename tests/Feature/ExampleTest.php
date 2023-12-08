c7868fb19fbef08f4c30235f635d5a76ad4dc731
mtrik <trikimabrouka09@gmail.com>
2023-12-08 12:08:28 +0100

    modifierencours

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,12 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_title_contains_hello_world(): void
     {
-        $response = $this->get('/');
 
+        $response = $this->get('/list/frais/1');
         $response->assertStatus(200);
+        $response->assertSee('Hello World!');
+
     }
 }
